ac15021d01622f016f9e6c3f10dbdb95b965c334
ee7klt <kltah1@gmail.com>
2014-06-25 03:59:26 +0800

    changed grid to 1D, xyto1D maps cartesian [1,N][1,N] to index [0,N^2-1]

--- Percolation.java
+++ Percolation.java
@@ -4,26 +4,46 @@ import java.util.Arrays;
 
 public class Percolation {
     
- private boolean[][] grid;   
- public Percolation(int N){ 
-      grid = new boolean[N][N];
-     System.out.println(Arrays.deepToString(grid));
- };   // create N-by-N grid, with all sites blocked
-public void open(int i, int j){
-    grid[i][j] = true;
-    System.out.println(Arrays.deepToString(grid));
+ private boolean[] grid;
+ private int N;
+ 
+ 
+ private int xyto1D(int i, int j) {    //maps cartesian i,j [1,N] to int [0,N^2-1]
+     int index;
+     index = (i-1)*N+(j-1);
+     System.out.println(index);
+     return index;   
+ }
+ 
+ public Percolation(int N){   // create N-by-N grid, with all sites blocked
+     this.N=N;
+      //grid = new boolean[(int)Math.pow(N,2) + 2];   //+2 for virtual sites on top and bottom to make detection fast
+     grid = new boolean[(int)Math.pow(N,2)];
+     System.out.println(Arrays.toString(grid));
+ };   
+
+ 
+ public void open(int i, int j){
+    grid[xyto1D(i,j)] = true;
+    System.out.println(Arrays.toString(grid));
 }    ;     // open site (row i, column j) if it is not already
-public boolean isOpen(int i, int j){System.out.println("hi");
-    return true;}  ;  // is site (row i, column j) open?
+public boolean isOpen(int i, int j){ // is site (row i, column j) open?
+    
+    System.out.println("hi");
+    return true;}  ;  
 public boolean isFull(int i, int j){System.out.println("hi");
     return true;} ;   // is site (row i, column j) full?
-public boolean percolates(){System.out.println("hi");
+public boolean percolates(){
+    
+    WeightedQuickUnionUF UF=new WeightedQuickUnionUF(N);
+    
+    System.out.println("hi");
     return true;}   ;          // does the system percolate?
 
 
 public static void main(String[] args) {
-    Percolation percolation = new Percolation(5);
-    percolation.open(1,1);
+    Percolation percolation = new Percolation(2);
+    percolation.open(2,2);
     
 }
 
