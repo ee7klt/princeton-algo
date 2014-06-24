bc8b0c112382b872f3c568806f8a65abe400a1cc
ee7klt <kltah1@gmail.com>
2014-06-24 11:08:05 +0800

    set up grid, open works

--- Percolation.java
+++ Percolation.java
@@ -3,11 +3,16 @@ import java.util.Arrays;
 
 
 public class Percolation {
+    
+ private boolean[][] grid;   
  public Percolation(int N){ 
-     boolean[][] grid = new boolean[N][N];
+      grid = new boolean[N][N];
      System.out.println(Arrays.deepToString(grid));
  };   // create N-by-N grid, with all sites blocked
-public void open(int i, int j){System.out.println("hi");}    ;     // open site (row i, column j) if it is not already
+public void open(int i, int j){
+    grid[i][j] = true;
+    System.out.println(Arrays.deepToString(grid));
+}    ;     // open site (row i, column j) if it is not already
 public boolean isOpen(int i, int j){System.out.println("hi");
     return true;}  ;  // is site (row i, column j) open?
 public boolean isFull(int i, int j){System.out.println("hi");
@@ -18,6 +23,7 @@ public boolean percolates(){System.out.println("hi");
 
 public static void main(String[] args) {
     Percolation percolation = new Percolation(5);
+    percolation.open(1,1);
     
 }
 
