8ce0f344484a101c0cdff2517b00eddae1924b40
ee7klt <kltah1@gmail.com>
2014-06-28 07:46:34 +0800

    implemented isOpen()

--- Percolation.java
+++ Percolation.java
@@ -29,41 +29,68 @@ public class Percolation {
  public Percolation(int N){   // create N-by-N grid, with all sites blocked
      this.N=N;
       //grid = new boolean[(int)Math.pow(N,2) + 2];   //+2 for virtual sites on top and bottom to make detection fast
-     grid = new boolean[(int)Math.pow(N,2)+2];
-      UF = new WeightedQuickUnionUF(N);
+     //create NxN grid + top and bottom virtual sites
+     //top virtual site has index 0
+     //bottom virtual site has index N^2+1
+     //grid sites have indices 1 to N^2
+     grid = new boolean[(int)Math.pow(N,2)+2];         
+      UF = new WeightedQuickUnionUF(N);                //define new UF data structure
      System.out.println(Arrays.toString(grid));
-     System.out.println(UF.connected(1,2));
+     //System.out.println(UF.connected(1,2));
      
-System.out.println(UF);
+
      
  };   
 
  
- public void open(int i, int j){
-     indexValidation(i,j);
-     grid[xyto1D(i,j)] = true;
-     
+ public void open(int i, int j){  // open site (row i, column j) if it is not already
+     indexValidation(i,j);       //is site within grid?
+     int thisSite = xyto1D(i,j);
+     grid[thisSite] = true;   //mark site as open
     System.out.println(Arrays.toString(grid));
-}    ;     // open site (row i, column j) if it is not already
+     
+    //check to see if neighboring sites are within grid.
+    //if so, check to see if they are open
+    //if so, connect them to present site
+    
+    if ((i-1) > 0) {
+          
+    };
+        
+    
+};    
+
+ 
 public boolean isOpen(int i, int j){ // is site (row i, column j) open?
     
-    System.out.println("hi");
-    return true;}  ;  
-public boolean isFull(int i, int j){System.out.println("hi");
+    boolean truthy = false;
+    indexValidation(i,j);
+    if (grid[xyto1D(i,j)] == true) { 
+        truthy = true;
+    }
+    System.out.println(truthy);
+    return truthy;
+};
+      
+
+
+
+public boolean isFull(int i, int j){
     return true;} ;   // is site (row i, column j) full?
 
 
 public boolean percolates(){
     
-    WeightedQuickUnionUF UF=new WeightedQuickUnionUF(N);
+    //WeightedQuickUnionUF UF=new WeightedQuickUnionUF(N);
     
-    System.out.println("hi");
+    
     return true;}   ;          // does the system percolate?
 
 
 public static void main(String[] args) {
     Percolation percolation = new Percolation(2);
     percolation.open(1,1);
+    percolation.isOpen(2,1);
     
     
 }
