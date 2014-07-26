018b2c889d23c3551bf27ec90194f67e91c5d58f
ee7klt <kltah1@gmail.com>
2014-07-26 19:24:07 +0800

    topdown

--- mergesort/Topdown.java
+++ mergesort/Topdown.java
@@ -1,9 +1,30 @@
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class merge {
 
-public static void merge (Comparable[] a, int lo, int mid, int hi)
+public class Topdown
+    
+{
+    private static Comparable[] aux;
+    
+    public static void sort(Comparable[] a)
+    {
+        aux = new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+    }
+    
+    private static void sort(Comparable[] a, int lo, int hi)
+    {
+        if (hi <= lo) return;
+        int mid = lo + (hi - lo)/2;
+        System.out.printf("lo = %i, mid = %i, hi = %i",lo,mid,hi);
+        sort(a, lo, mid);
+        sort(a, mid+1, hi);
+        merge(a, lo, mid, hi);
+    }
+        
+    
+    public static void merge (Comparable[] a, int lo, int mid, int hi)
 {
     int i = lo, j = mid+1;
     
@@ -39,8 +60,8 @@ public static void merge (Comparable[] a, int lo, int mid, int hi)
 private static boolean less(Comparable v, Comparable w) {
         return (v.compareTo(w) < 0);
     }
-        
-        
+
+
 public static void main(String[] args) {
     Comparable[] a  = {2,5,6,1,3,9};
     //System.out.println(Arrays.toString(a));
@@ -49,9 +70,6 @@ public static void main(String[] args) {
  
 
 }
-
-
-
+        
+        
 }
-
-
