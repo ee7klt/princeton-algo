a709b30f025b63ba725044026e8f36e2c5e11355
ee7klt <kltah1@gmail.com>
2014-08-02 16:06:18 +0800

    Starting Brute.java 2

--- pattern/Brute.java
+++ pattern/Brute.java
@@ -15,18 +15,30 @@ public class Brute {
             points[i] = p;
         }
         
+        
+        
+        //put points in lexicographic order
+        
+        
+        //for each point, compute slope with respect to all other points.
+        //if similar slopes found, flip index of that point in pointsTruthy. 
         for (int i = 0; i < points.length; i++) {
             System.out.println(points[i]);
            Point point1 = points[i];
            int count = 0;
            double oldSlope = 0;
            Boolean[] pointsTruthy = new Boolean[points.length];
-           for (
+           
+           for (int k = 0; k < points.length; k++) {
+               pointsTruthy[k] = false;
+           
+           }
             for (int j = 0; j < points.length; j++) {
                Point point2 = points[j];
                double newSlope = point1.slopeTo(point2);
                if (newSlope == oldSlope) {
                   ++count;
+                  pointsTruthy[i] = true;
                   
                }
                oldSlope = newSlope;
