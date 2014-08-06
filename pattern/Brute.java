905f7883be3fe3945f86480821fa13be43b27480
ee7klt <kltah1@gmail.com>
2014-08-06 19:30:40 +0800

    factor out the tuples before computing slopes

--- pattern/Brute.java
+++ pattern/Brute.java
@@ -3,6 +3,25 @@ import java.util.Arrays;
 
 public class Brute {
     
+    
+   /**
+    * find out if the tuples are collinear
+    */
+    
+    public boolean checkSlopes(Point p1, Point p2, Point p3, Point p4) {
+   
+     Boolean truthy = false;
+     
+     
+   
+        
+        return truthy;
+    }
+    
+    
+    
+         
+    
  
     
     public static void main(String[] args) {
@@ -20,32 +39,27 @@ public class Brute {
         
         
         
-        //put points in lexicographic order
+       
         
         
-        //for each point, compute slope with respect to all other points.
-        //if similar slopes found, flip index of that point in pointsTruthy. 
-        for (int i = 0; i < points.length; i++) {
-            System.out.println(points[i]);
-           Point point1 = points[i];
-           int count = 0;
-           double oldSlope = 0;
-           double[] slopes = new double[points.length];
-           
-           for (int k = 0; k < points.length; k++) {
-               slopes[k] = 0.;
-           
-           }
-            for (int j = 0; j < points.length; j++) {
-               Point point2 = points[j];
-               double slope = point1.slopeTo(point2);
-                slopes[j] = slope;
-
-  
+        
+        //find combinations of 4-tuples and check if they are collinear
+        
+      
+        for (int i=0; i < points.length; i++) {
+            for (int j=0; j<i; j++) {
+                for (int k = 0; k < j; k++) {
+                    for (int l = 0; l < k; l++) { 
+                        Point[] tuple = {points[i],points[j],points[k],points[l]};
+                        
+                    }
+                }
             }
-            System.out.println(Arrays.toString(slopes));
-            
         }
+        
+       
+         
+   
         
         
     
